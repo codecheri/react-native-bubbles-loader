b2a9762dd8ee1e04012866dfe78df47d29a77f5c
codecheri <54028059+codecheri@users.noreply.github.com>
2020-04-18 11:22:01 +0200

    Implicit import

--- src/DoubleBounce.js
+++ src/DoubleBounce.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Animated } from 'react-native';
-import ART from '@react-native-community/art';
+import { ART } from '@react-native-community/art';
 const { Surface } = ART;
 
 import Circle from './animated/Circle';
